aa7485ae9641e7f548b93dd95f38ada5f641f8a9
Natasha Minako <adrielynatashama@hotmail.com>
2025-03-18 11:52:19 -0300

    Java

---
+++ Exams/Exams.java
@@ -0,0 +1,19 @@
+package Exams;
+public class Exams {
+    int idExams;
+    int glucoseLevel;
+
+    String getDiagnosis() {
+    if (glucoseLevel < 100) {
+    return "Normal";
+
+    } else if (glucoseLevel <= 125) {
+    return "Prediabetes";
+
+    } else {
+	return "Diabetes";
+
+    }
+	}
+}
+
