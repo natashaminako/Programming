e69292b9d47159e80b4477841dab2ba2509a34ce
Natasha Minako <adrielynatashama@hotmail.com>
2025-03-20 18:31:31 -0300

    Java

--- AppTestAccountBank/TestAccountBank.java
+++ AppTestAccountBank/TestAccountBank.java
@@ -1,5 +1,4 @@
 package AppTestAccountBank;
-
 public class TestAccountBank {
 	private int AccountID;
 	private String agency;
@@ -9,45 +8,45 @@ public class TestAccountBank {
 	private double creditlimit;
 
 	public AccountBank(int AccountID, String agency, String accountNumber, char type, double balance, double creditLimit) {
-		this.AccountID = AccountID;
-		this.agency = agency;
-		this.accountNumber = accountNumber;
-		this.type = 'N';
-		this.balance = 0.0;
-		this.creditlimit = 0.0;
+	this.AccountID = AccountID;
+	this.agency = agency;
+	this.accountNumber = accountNumber;
+	this.type = 'N';
+	this.balance = 0.0;
+	this.creditlimit = 0.0;
 	}
 
 	public AccountBank(int AccountID, String agency, String accountNumber, chat type, double balance, double creditLimit) {
-		this.AccountID = AccountID;
-		this.agency = agency;
-		this.accountNumber = accountNumber;
-		this.type = 'S';
-		this.balance = 0.0;
-		this.creditLimit = creditLimit;
+	this.AccountID = AccountID;
+	this.agency = agency;
+	this.accountNumber = accountNumber;
+	this.type = 'S';
+	this.balance = 0.0;
+	this.creditLimit = creditLimit;
 	}
 
 	public boolean changeLimit(double balance) {
-		if (type == 'S') {
-			if (balance == 0) {
-				type = 'N';
-				creditLimit = 0.0;
-			} else {
-				creditLimit = balance;
-			}
-			return true;
-		}
-		return false;
+	if (type == 'S') {
+	if (balance == 0) {
+	type = 'N';
+	creditLimit = 0.0;
+	} else {
+	creditLimit = balance;
+	}
+	return true;
+	}
+	return false;
 	}
 
 	public void registerDeposit(double balance) {
-		if (balance > 0) {
-			if (type == 'N') {
-				if (0.0 >= -creditlimit) {
-					balance -= balance;
-					return true;
-				}
-			}
-		}
+	if (balance > 0) {
+	if (type == 'N') {
+	if (0.0 >= -creditlimit) {
+	balance -= balance;
+	return true;
+	}
+	}
+	}
 	}
 	return false;
 }
