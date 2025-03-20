d06e2d143968f3d00fae0c11df344dd45352df69
Natasha Minako <adrielynatashama@hotmail.com>
2025-03-20 18:27:15 -0300

    Java

--- AppTestAccountBank/AccountBank.java
+++ AppTestAccountBank/AccountBank.java
@@ -1,44 +1,44 @@
 package AppTestAccountBank;
 
-public class AccountBank {
+	public class AccountBank {
 	private String accountID;
 	private String agency;
 	private String type;
 	private String accountNumber;
 	private double balance;
-}
+	}
 
-public AccountBank(String accountID, String agency, String type, String accountNumber, double balance) {
+	public AccountBank(String accountID, String agency, String type, String accountNumber, double balance) {
 	this.accountID = accountID;
 	this.agency = agency;
 	this.type = type;
 	this.accountNumber;
 	this.balance = balance;
-}
+	}
 
-public String getAccountID() {
+	public String getAccountID() {
 	return accountID;
-}
+	}
 
-public String getagency() {
+	public String getagency() {
 	return agency;
-}
+	}
 
-public Strin gettype() {
+	public Strin gettype() {
 	return type;
-}
+	}
 
-public String getAccountNumber() {
+	public String getAccountNumber() {
 	return accountNumber;
-}
+	}
 
-public double getBalance() {
+	public double getBalance() {
 	return balance;
-}
+	}
 
-public boolean registerDeposit(double balance) {
+	public boolean registerDeposit(double balance) {
 	if (balance > 0) {
-		this.balance += balance;
-		return true;
+	this.balance += balance;
+	return true;
 	}
 }
